32f61f32fc6b1c73e5f2e189f0fa870f8eecde97
Joakim Bech <joakim.bech@linaro.org>
2015-04-29 15:23:15 +0200

    Updates according to updates in dma_buf_export
    In later kernel versions (after Jan 2015), the dma_buf_export(...)
    function has been changed from having several parameters to instead have
    a structure containing all those parameters as the only argument. This
    patch enables use of both the old and the new way of calling the
    function.
    
    For travis we changed from using Linaro LSK to use Linux from Linus
    official tree instead.
    
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>
    Tested-by: Joakim Bech <joakim.bech@linaro.org> (QEMU, 3.18rc1, 4.1rc1)
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

--- core/tee_shm.c
+++ core/tee_shm.c
@@ -360,9 +360,20 @@ static int export_buf(struct tee *tee, struct tee_shm *shm, int *export)
 {
 	struct dma_buf *dmabuf;
 	int ret = 0;
+	/* Temporary fix to support both older and newer kernel versions. */
+#if defined(DEFINE_DMA_BUF_EXPORT_INFO)
+	DEFINE_DMA_BUF_EXPORT_INFO(exp_info);
 
+	exp_info.priv = shm;
+	exp_info.ops = &_tee_shm_dma_buf_ops;
+	exp_info.size = shm->size_alloc;
+	exp_info.flags = O_RDWR;
+
+	dmabuf = dma_buf_export(&exp_info);
+#else
 	dmabuf = dma_buf_export(shm, &_tee_shm_dma_buf_ops, shm->size_alloc,
 				O_RDWR, 0);
+#endif
 	if (IS_ERR_OR_NULL(dmabuf)) {
 		dev_err(_DEV(tee), "%s: dmabuf: couldn't export buffer (%ld)\n",
 			__func__, PTR_ERR(dmabuf));
